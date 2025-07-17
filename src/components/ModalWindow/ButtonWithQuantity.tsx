7f6ee551e37380f68907231ca18857430a62077a
lahodiukoleksandra <81188068+lahodiukoleksandra@users.noreply.github.com>
2025-07-17 17:00:03 +0200

    Add styles to ButtonWithQuantity/SetWithQuantity

--- src/components/ModalWindow/ButtonWithQuantity.tsx
+++ src/components/ModalWindow/ButtonWithQuantity.tsx
@@ -1,40 +1,52 @@
 "use client";
 import { Inter } from "next/font/google";
 import ToggleIncrease from "./ToggleIncrease";
+import { colors } from "../../../lib/colors";
 
 const inter = Inter({ subsets: ["latin"], weight: "400" });
 const inter2 = Inter({ subsets: ["latin"], weight: "600" });
 
 type ButtonWithQuantityProps = {
-    price: number;
-    quantity: number;
-    setQuantity: React.Dispatch<React.SetStateAction<number>>;
-    allowZero?: boolean;
+  price: number;
+  quantity: number;
+  setQuantity: React.Dispatch<React.SetStateAction<number>>;
+  allowZero?: boolean;
 };
 
-export default function ButtonWithQuantity({ 
-    price, 
-    quantity, 
-    setQuantity, 
-    allowZero = false 
+export default function ButtonWithQuantity({
+  price,
+  quantity,
+  setQuantity,
+  allowZero = false,
 }: ButtonWithQuantityProps) {
-    const singlePrice = price
-    
-    return (
-        <div className="flex justify-between items-center mb-6">
-            <div>
-                <p className={`${inter.className} text-gray-400 text-[12px]`}>Czas dostawy</p>
-                <p className={`${inter2.className} text-white text-[16px] mt-1`}>30 min</p>
-            </div>
-            <div>
-                <p className={`${inter.className} text-gray-400 text-[12px]`}>Suma</p>
-                <p className={`${inter2.className} text-white text-[16px] mt-1`}>{singlePrice}zł</p>
-            </div>
-            <ToggleIncrease 
-                quantity={quantity} 
-                setQuantity={setQuantity} 
-                allowZero={allowZero}
-            />
-        </div>
-    );
+  const singlePrice = price;
+
+  return (
+    <div className="relative flex justify-between items-center mb-6 px-4 py-6 overflow-hidden">
+      <div
+        className="absolute w-[50px] h-[50px] rounded-full blur-lg opacity-80 left-4 top-4 z-0"
+        style={{ backgroundColor: colors.zieliony }}
+      ></div>
+      <div
+        className="absolute w-[50px] h-[50px] rounded-full blur-lg opacity-60 left-30 top-2 z-0"
+        style={{ backgroundColor: colors.zieliony }} ></div>
+      <div className="z-10">
+        <p className={`${inter.className} text-white text-[12px]`}>Czas dostawy</p>
+        <p className={`${inter2.className} text-white text-[16px] mt-1`}>30 min</p>
+      </div>
+      <div className="z-10">
+        <p className={`${inter.className} text-white text-[12px]`}>Suma</p>
+        <p className={`${inter2.className} text-white text-[16px] mt-1`}>
+          {singlePrice}zł
+        </p>
+      </div>
+      <div className="z-10">
+        <ToggleIncrease
+          quantity={quantity}
+          setQuantity={setQuantity}
+          allowZero={allowZero}
+        />
+      </div>
+    </div>
+  );
 }
--- src/components/ModalWindow/SetWithQuantity.tsx
+++ src/components/ModalWindow/SetWithQuantity.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"], weight: ["400"] });
 const inter2 = Inter({ subsets: ["latin"], weight: "600" });
 
 type Props = {
-    name: string,
+    name: string;
     price: number;
     quantity: number;
     setQuantity: (value: number) => void;
@@ -14,23 +14,34 @@ type Props = {
 
 export default function SetWithQuantity({ name, price, quantity, setQuantity }: Props) {
     const singleSetPrice = quantity > 0 ? price / quantity : price;
-    const zestawPrice = price + 7
-    
+    const zestawPrice = price + 7;
+
     return (
-        <>
-            <h3 className={`${inter2.className} text-[20px] mb-2`}>Zestaw</h3>
-            <h2 className={`${inter.className} text-[14px] mb-2 text-grayI`}>Zestaw {name}, z frytkami i ketchupem</h2>
-            <div className="flex justify-between items-center mt-6">
+        <div className="relative">
+            <div className="absolute rounded-full h-[60px] w-[80px] bg-[#68FF3A] top-0 left-0 blur-lg z-0 opacity-70"></div>
+            <h3 className={`${inter2.className} text-[20px] mb-2 z-10 relative`}>Zestaw</h3>
+            <h2 className={`${inter.className} text-[14px] mb-2 text-grayI relative z-10`}>
+                Zestaw {name}, z frytkami i ketchupem
+            </h2>
+
+            <div className="flex justify-between items-center mt-6 relative z-10">
                 <div>
                     <p className={`${inter.className} text-grayI text-[12px]`}>Czas dostawy</p>
                     <p className={`${inter2.className} text-white text-[16px] mt-1`}>30 min</p>
                 </div>
                 <div>
                     <p className={`${inter.className} text-grayI text-[12px]`}>Suma</p>
-                    <p className={`${inter2.className} text-white text-[16px] mt-1`}>{zestawPrice}zł</p>
+                    <div className="absolute rounded-full h-[50px] w-[60px] bg-[#68FF3A] top-0 left-63 blur-lg z-0 opacity-70"></div>
+                    <p className={`${inter2.className} text-white text-[16px] mt-1`}>
+                        {zestawPrice}zł
+                    </p>
                 </div>
-                <ToggleIncrease quantity={quantity} setQuantity={setQuantity} allowZero={true} />
+                <ToggleIncrease
+                    quantity={quantity}
+                    setQuantity={setQuantity}
+                    allowZero={true}
+                />
             </div>
-        </>
+        </div>
     );
 }
