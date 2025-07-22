9a6edc96ae922a667352c0de962de5e9343015ae
lahodiukoleksandra <81188068+lahodiukoleksandra@users.noreply.github.com>
2025-07-22 12:37:40 +0200

    Made modal window responsible

--- src/components/ModalWindow/ButtonWithQuantity.tsx
+++ src/components/ModalWindow/ButtonWithQuantity.tsx
@@ -6,35 +6,41 @@ const inter = Inter({ subsets: ["latin"], weight: "400" });
 const inter2 = Inter({ subsets: ["latin"], weight: "600" });
 
 type ButtonWithQuantityProps = {
-    price: number;
-    quantity: number;
-    setQuantity: React.Dispatch<React.SetStateAction<number>>;
-    allowZero?: boolean;
+  price: number;
+  quantity: number;
+  setQuantity: React.Dispatch<React.SetStateAction<number>>;
+  allowZero?: boolean;
 };
 
-export default function ButtonWithQuantity({ 
-    price, 
-    quantity, 
-    setQuantity, 
-    allowZero = false 
+export default function ButtonWithQuantity({
+  price,
+  quantity,
+  setQuantity,
+  allowZero = false,
 }: ButtonWithQuantityProps) {
-    const singlePrice = price
-    
-    return (
-        <div className="flex justify-between items-center mb-6">
-            <div>
-                <p className={`${inter.className} text-gray-400 text-[12px]`}>Czas dostawy</p>
-                <p className={`${inter2.className} text-white text-[16px] mt-1`}>30 min</p>
-            </div>
-            <div>
-                <p className={`${inter.className} text-gray-400 text-[12px]`}>Suma</p>
-                <p className={`${inter2.className} text-white text-[16px] mt-1`}>{singlePrice}zł</p>
-            </div>
-            <ToggleIncrease 
-                quantity={quantity} 
-                setQuantity={setQuantity} 
-                allowZero={allowZero}
-            />
-        </div>
-    );
+  const singlePrice = price;
+
+  return (
+    <div className="relative flex justify-between items-center md:px-4 md:py-6 md:mb-[2px] mb-6 overflow-hidden">
+      <div className="absolute w-[50px] h-[50px] rounded-full blur-lg opacity-80 left-5 top-5 z-0 bg-[#68FF3A] hidden md:block"></div>
+      <div className="absolute w-[50px] h-[50px] rounded-full blur-lg opacity-70 left-30 top-5 z-0 bg-[#68FF3A] hidden md:block"></div>
+      <div className="z-10">
+        <p className={`${inter.className} text-white md:text-[12px] text-[10px]`}>Czas dostawy</p>
+        <p className={`${inter2.className} text-white md:text-[16px] text-[12px] mt-1`}>30 min</p>
+      </div>
+      <div className="z-10">
+        <p className={`${inter.className} text-white md:text-[12px] text-[10px]`}>Suma</p>
+        <p className={`${inter2.className} text-white md:text-[16px] text-[12px] mt-1`}>
+          {singlePrice}zł
+        </p>
+      </div>
+      <div className="z-10">
+        <ToggleIncrease
+          quantity={quantity}
+          setQuantity={setQuantity}
+          allowZero={allowZero}
+        />
+      </div>
+    </div>
+  );
 }
--- src/components/ModalWindow/SetWithQuantity.tsx
+++ src/components/ModalWindow/SetWithQuantity.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { Inter } from "next/font/google";
 import ToggleIncrease from "./ToggleIncrease";
+import { colors } from "../../../lib/colors";
 
 const inter = Inter({ subsets: ["latin"], weight: ["400"] });
 const inter2 = Inter({ subsets: ["latin"], weight: "600" });
 
 type Props = {
-    name: string,
+    name: string;
     price: number;
     quantity: number;
     setQuantity: (value: number) => void;
@@ -14,23 +15,37 @@ type Props = {
 
 export default function SetWithQuantity({ name, price, quantity, setQuantity }: Props) {
     const singleSetPrice = quantity > 0 ? price / quantity : price;
-    const zestawPrice = price + 7
-    
+    const zestawPrice = price + 7;
+
     return (
-        <>
-            <h3 className={`${inter2.className} text-[20px] mb-2`}>Zestaw</h3>
-            <h2 className={`${inter.className} text-[14px] mb-2 text-grayI`}>Zestaw {name}, z frytkami i ketchupem</h2>
-            <div className="flex justify-between items-center mt-6">
-                <div>
-                    <p className={`${inter.className} text-grayI text-[12px]`}>Czas dostawy</p>
-                    <p className={`${inter2.className} text-white text-[16px] mt-1`}>30 min</p>
-                </div>
-                <div>
-                    <p className={`${inter.className} text-grayI text-[12px]`}>Suma</p>
-                    <p className={`${inter2.className} text-white text-[16px] mt-1`}>{zestawPrice}zł</p>
-                </div>
-                <ToggleIncrease quantity={quantity} setQuantity={setQuantity} allowZero={true} />
+        <div className="relative md:px-4 md:py-6 md:mb-[2px] mb-6 overflow-hidden">
+            <div className="absolute rounded-full h-[60px] w-[80px] bg-[#68FF3A] top-5 left-5 blur-lg z-0 opacity-70 hidden md:block"></div>
+            <div className="absolute rounded-full h-[50px] w-[50px] bg-[#68FF3A] top-20 left-30 blur-lg z-0 opacity-70 hidden md:block"></div>
+            <div className="relative z-10 mb-4">
+                <h3 className={`${inter2.className} text-[20px] mb-2`}>Zestaw</h3>
+                <h2 className={`${inter.className} text-[14px] text-grayI`}>
+                    Zestaw {name}, z frytkami i ketchupem
+                </h2>
             </div>
-        </>
+            <div className="flex justify-between items-center relative z-10">
+                <div>
+                    <p className={`${inter.className} text-white md:text-[12px] text-[10px]`}>Czas dostawy</p>
+                    <p className={`${inter2.className} text-white md:text-[16px] text-[12px] mt-1`}>30 min</p>
+                </div>
+                <div>
+                    <p className={`${inter.className} text-white md:text-[12px] text-[10px]`}>Suma</p>
+                    <p className={`${inter2.className} text-white md:text-[16px] text-[12px] mt-1`}>
+                        {zestawPrice}zł
+                    </p>
+                </div>
+                <div>
+                    <ToggleIncrease
+                        quantity={quantity}
+                        setQuantity={setQuantity}
+                        allowZero={true}
+                    />
+                </div>
+            </div>
+        </div>
     );
 }
