0e3ab42c452f2b89ed5da2449ebac890ec1d47dc
lahodiukoleksandra <81188068+lahodiukoleksandra@users.noreply.github.com>
2025-08-12 20:05:54 +0200

    Styling ListOfProduct, add additionals for description

---
+++ src/components/Basket/BackgroundBlobsCart.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function BackgroundBlobsCart() {
+    return (
+        <>
+        <div className="w-[580px] h-[573px] bg-gradient-r from-[#E01094] to-[#7A0950] z-999 rounded-full"></div>
+        </>
+    )
+}
--- src/components/Basket/Basket.tsx
+++ src/components/Basket/Basket.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import StepButtons from "./StepButtons";
 import ListOfProducts from "./ListOfProducts"
+import BackgroundBlobsCart from "./BackgroundBlobsCart";
 
 export default function BasketPage() {
   const [currentStep, setCurrentStep] = useState(1);
@@ -12,6 +13,8 @@ export default function BasketPage() {
       <ListOfProducts currentStep={currentStep} />
       {currentStep === 2 && <div>Formularz danych kontaktowych</div>}
       {currentStep === 3 && <div>Wybór metody płatności</div>}
+      <div className="w-[580px] h-[573px] bg-gradient-r from-[#E01094] to-[#7A0950] z-999 rounded-full"></div>
+      <BackgroundBlobsCart />
     </div>
   );
 }
