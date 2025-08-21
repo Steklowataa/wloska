d8e6ba4f4c0d4649cad3997eeda220b8b898c771
lahodiukoleksandra <81188068+lahodiukoleksandra@users.noreply.github.com>
2025-08-21 15:43:38 +0200

    Update text.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,35 +5,14 @@ import SpinningEllipse from "@/components/Home/SpinEllipse";
 import EffectAppear from "@/components/Home/EffectAppear";
 import ImageAppear from "@/components/Home/ImageAppear";
 import Header from "@/components/Header";
+import { text } from "../utils/text"
 
 
 export default function Home() {
     const [currentIndex, setCurrentIndex] = useState(0);
+    const currentSecondHeader = text.secondHeader[currentIndex]
 
-    const images = [
-      "/images/bianca.svg",
-      "/images/z-gruszka.svg",
-      "/images/4-sery.png",
-      "/images/4-miesa.svg"
-    ]
-
-    const pizzaData = [
-        { name: "Salami", subtitle: "Bianca", description: "...", color: '#E01094' },
-        { name: "Pizza z", subtitle: "Gruszką", description: "...", color: '#940C62' },
-        { name: "Pizza", subtitle: "Cezar", description: "...", color: '#43A9F7' },
-        { name: "Quatro", subtitle: "Carni", description: "...", color: '#0F7BCE' }
-    ];
-
-    const secondHeader = [
-      {text: "Salami Bianca to idealna propozycja dla fanów intensywnych smaków i kremowej bazy. Na delikatnym sosie śmietanowym układamy aromatyczne pieczarki"},
-      {text: "Pizza z Gruszką to wyjątkowe połączenie delikatności i charakteru. Na kremowym sosie układamy soczyste plastry gruszki, rukolę i gorgonzolę, całość"},
-      {text: "Pizza Cezar to wariacja na temat klasycznej sałatki. Na sosie śmietanowym znajdziesz kawałki soczystego kurczaka, czarne oliwki i czerwoną cebulkę, "},
-      {text: "Quatro Carni to propozycja dla miłośników mięsnych smaków. Na bazie aromatycznego sosu znajdziesz szynkę, pikantną spianatę, chrupiące frankfurterki"}
-    ]
-
-    const currentSecondHeader = secondHeader[currentIndex]
-
-    const currentPizza = pizzaData[currentIndex];
+    const currentPizza = text.header[currentIndex];
 
     const firstTextTime = currentPizza.name.length * 200; // speed 
     const secondTextDelay = 1000; // delay dla drugiego słowa
@@ -42,7 +21,7 @@ export default function Home() {
 
     useEffect(() => {
         const timer = setTimeout(() => {
-            setCurrentIndex((prev) => (prev + 1) % pizzaData.length);
+            setCurrentIndex((prev) => (prev + 1) % text.header.length);
         }, totalTime);
 
         return () => clearTimeout(timer);
@@ -53,27 +32,27 @@ export default function Home() {
             <Header />
             <SpinningEllipse />
             <div>
-              <ImageAppear src={images[currentIndex]} trigger={currentIndex} duration={1000}/>
+              <ImageAppear src={text.images[currentIndex].img} trigger={currentIndex} duration={1000}/>
             </div>
             <div className="mt-[200px] ml-[100px] relative z-10">
                 <div className="text-[80px] font-bold leading-tight min-h-[160px]">
                     <div>
                         <TypingText 
                             speed={80} 
-                            text={currentPizza.name} 
+                            text={text.header[currentIndex].name} 
                             trigger={currentIndex}
                         />
                     </div>
-                    <div className="ml-10 min-h-30" style={{ color: currentPizza.color }}>
+                    <div className="ml-10 min-h-30" style={{ color: text.header[currentIndex].color }}>
                         <TypingText 
                             speed={80} 
-                            text={currentPizza.subtitle} 
+                            text={text.header[currentIndex].subtitle} 
                             delay={500} 
                             trigger={currentIndex}
                         />
                     </div>
                     <div className="w-[500px] text-[16px]">
-                      <EffectAppear text={currentSecondHeader.text} trigger={currentIndex}/>
+                      <EffectAppear text={text.secondHeader[currentIndex].text} trigger={currentIndex}/>
                     </div>
                 </div>
             </div>
