b7cd3f9d56233d1871486ba74d66fd87feb46c53
lahodiukoleksandra <81188068+lahodiukoleksandra@users.noreply.github.com>
2025-10-31 13:07:27 +0100

    Fix basket

--- src/app/context/CartContext.tsx
+++ src/app/context/CartContext.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { CartItem } from "@/utils/type";
 
 type CartContextType = {
@@ -13,6 +13,25 @@ const CartContext = createContext<CartContextType | undefined>(undefined);
 
 export function CartProvider({ children }: { children: React.ReactNode }) {
   const [items, setItems] = useState<CartItem[]>([]);
+  const [isHydrated, setIsHydrated] = useState(false);
+
+  useEffect(() => {
+    try {
+      const storedItems = localStorage.getItem("cartItems");
+      if (storedItems) {
+        setItems(JSON.parse(storedItems));
+      }
+    } catch (error) {
+      console.error("Error reading cart items from localStorage", error);
+    }
+    setIsHydrated(true);
+  }, []);
+
+  useEffect(() => {
+    if (isHydrated) {
+      localStorage.setItem("cartItems", JSON.stringify(items));
+    }
+  }, [items, isHydrated]);
 
   const addToCart = (item: CartItem) => {
     setItems((prevItems) => {
